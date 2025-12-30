99461b7b5ac7642bab06ea9920c8996d27a6d5a4
Debasish Sarkar <debasishsarkar452@gmail.com>
2025-12-30 21:43:36 +0530

    added code for latency

--- process_query.py
+++ process_query.py
@@ -1,8 +1,8 @@
 import requests
 import numpy as np
-import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import joblib
+import time
 
 def merged_func(q):
     def create_embeddings(text_list):
@@ -26,13 +26,17 @@ def merged_func(q):
     
     df = joblib.load("embeddings.joblib")
 
+    t0 = time.time()
     query = q
     emb_query = create_embeddings([query])[0]
+    embed_time = time.time() - t0
 
+    t1 = time.time()
     similarities = cosine_similarity(np.vstack(df["embedding"]), [emb_query]).flatten()
     top_results = 6
     max_idx = np.argsort(similarities)[::-1][:top_results]
     new_df = df.loc[max_idx]
+    retreival_time = time.time() - t1
 
     prompt = f'''Here are some video subtitle chunks containing video number, title, start time in seconds, end time in seconds and text content of that time chunk:
     {new_df[["number", "title", "start", "end", "text"]].to_json(orient="records")}
@@ -40,6 +44,15 @@ def merged_func(q):
     "{query}"
     User asked this question related to the video chunks, you have to answer in a human way (dont mention the above format, its just for you) where and how much content is taught in which video (in which video and at what timestamp) and guide the user to go to that particular video. If user asks unrelated question, tell him that you can only answer questions related to the course. And convert the seconds into minutes. You should also provide a summy of the topic.'''
 
+    t2 = time.time()
     response = inference(prompt)["response"]
+    llm_time = time.time() - t2
+
+    print({
+        "embed_time": round(embed_time, 4),
+        "retreival_time": round(retreival_time, 4),
+        "llm_time": round(llm_time, 4),
+        "tottal_time": round(embed_time + retreival_time + llm_time, 4)
+    })
 
     return response
